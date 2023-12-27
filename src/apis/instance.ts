a39c65da7813764cf9ddf3ac7094f15e92ed0769
truewayy <dldudwls888@gmail.com>
2023-12-27 20:57:40 +0900

    fix: instance return resopnse 값 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -11,7 +11,7 @@ const instance = axios.create({
 
 instance.interceptors.response.use(
   (response) => {
-    return response.data;
+    return response;
   },
   (error) => {
     return Promise.reject(error);
