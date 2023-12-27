f32a275ae70a298aa6cbda01cbbf7b9f74e32505
truewayy <dldudwls888@gmail.com>
2023-12-27 20:55:58 +0900

    feat: queryClient Provider 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,29 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
