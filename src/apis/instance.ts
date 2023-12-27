39a50fbd00f08a63b1117bb4a71fc05b0b8963f7
truewayy <dldudwls888@gmail.com>
2023-12-27 22:11:12 +0900

    fix: instance timeout (1s -> 5s)

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = 'https://api.github.com/repos';
 const instance = axios.create({
   baseURL: baseUrl,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
