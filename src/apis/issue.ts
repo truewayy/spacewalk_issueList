1a3d539cb5b797fb6a033930a253bbf3cc20297c
truewayy <dldudwls888@gmail.com>
2023-12-27 20:57:14 +0900

    fix: 이슈리스트 api함수 파라미터 수정

--- src/apis/issue.ts
+++ src/apis/issue.ts
@@ -1,9 +1,23 @@
 import { Issue } from '../interfaces/issue';
 import instance from './instance';
 
-export const getIssues = async (owner: string, repo: string) => {
+export const getIssues = async (
+  owner: string,
+  repo: string,
+  state: Issue['state'] = 'all',
+  sort: 'created' | 'updated' | 'comments' = 'created',
+  page = 1,
+  per_page = 10,
+) => {
   try {
-    const data = await instance.get<Issue[]>(`/${owner}/${repo}/issues`);
+    const { data } = await instance.get<Issue[]>(`/${owner}/${repo}/issues`, {
+      params: {
+        state,
+        sort,
+        page,
+        per_page,
+      },
+    });
     return data;
   } catch (error) {
     console.error(error);
