4a91f8a61fb19113c5c68cabc883d7dfed419591
truewayy <dldudwls888@gmail.com>
2023-12-27 20:30:18 +0900

    feat: 메인 페이지 url 설정 (/home)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter basename='/home'>
+      <Routes>
+        <Route path='/' element={<div />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
